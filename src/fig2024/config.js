3979a812c4cc84805301153b55c56043fed5c08f
LONGOUR Lucas (Eurométropole) <lucas.longour@strasbourg.eu>
2024-09-19 16:30:55 +0200

    [fig2024] switch jpg to webp

--- src/fig2024/config.js
+++ src/fig2024/config.js
@@ -67,7 +67,7 @@ var config = {
         {
             id: '1',
             title: 'Revalorisation des friches industrielles',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/01_Fischer.jpg': { "alt": "01_Fischer", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/01_Fischer.webp': { "alt": "01_Fischer", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'Avec les mutations économiques de la fin du 20e siècle, de nombreux sites industriels ont vu leur activité disparaître. Afin de revaloriser ce patrimoine bâti, l’Eurométropole de Strasbourg et ses communes ont initié plusieurs projets visant à décontaminer et réaffecter ces friches. A titre d’exemple, à Reichstett, l’ancienne raffinerie a laissé place à l’EcoParc Rhénan, un parc d’activités de 85 hectares pour les secteurs de l’industrie, de l’artisanat et du service. A Strasbourg, le projet urbain Deux-Rives / Zwei-Ufer vise la revalorisation de 250 hectares de friches portuaires en un véritable quartier, mêlant logements, commerces, lieux de travail et espaces verts. A Schiltigheim, l’ancienne brasserie Fischer fait l’objet d’une requalification en secteur à vocation mixte, ayant pour objectif d’augmenter l’offre résidentielle au sud de la ville, de mettre en valeur le patrimoine industriel brassicole, et de proposer des activités économiques et de loisirs créatrices d’emplois.',
             location: {
                 center: [7.742632, 48.602402],
@@ -91,7 +91,7 @@ var config = {
         {
             id: '2',
             title: 'Des pôles d’innovation technologique',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/02_Nextmed.jpg': { "alt": "02_Nextmed", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/02_Nextmed.webp': { "alt": "02_Nextmed", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'A la fin des années 1980, la Communauté urbaine de Strasbourg consacre au sud de l’agglomération une cinquantaine d’hectares à la recherche et à l’enseignement supérieur scientifique et aux activités de haute technologie. Au fil du temps, le Parc d’innovation d’Illkirch s’agrandit et accroit son rayonnement, accueillant aujourd’hui plus d’une centaine d’entreprises internationales sur près de 170 hectares. Avec l’inauguration du Nouvel Hôpital Civil en 2009, les efforts de la Ville et de l’Eurométropole de Strasbourg se focalisent sur le site en cœur de métropole de l’ancien hôpital civil. Spécialisé dans le domaine de l’innovation en santé, nextmed accueille aujourd’hui plus d’une cinquantaine de sociétés sur une trentaine d’hectares.',
             location: {
                 center: [7.743709, 48.576721],
@@ -105,7 +105,7 @@ var config = {
         {
             id: '3',
             title: 'La zone d’activité agricole d’Holtzheim : vers une agriculture biologique de proximité',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/03_Holtzheim.jpg': { "alt": "03_Holtzheim", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/03_Holtzheim.webp': { "alt": "03_Holtzheim", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'Le projet alimentaire territorial de la Ville et Eurométropole de Strasbourg, a pour objectif de développer une agriculture durable et une alimentation de qualité, accessible à tous. Parmi les actions en cours, les Zones d’Activité Agricole concrétisent une volonté d’abandonner des anciens projets d’urbanisation aux bénéfices d’une transition agricole et alimentaire, en ciblant quatre zones prioritaires : à la Robertsau, dans un secteur situé dans les communes de Souffelweyersheim, Hoenheim et Bischheim, à Illkirch-Graffenstaden, et dans le parc d’activité nord aéroport de Holtzheim. Ce dernier a récemment accueilli un premier projet de maraîchage biologique de 5 hectares, qui s’accompagne de la création d’emplois en insertion.',
             location: {
                 center: [7.641234, 48.553143],
@@ -119,7 +119,7 @@ var config = {
         {
             id: '4',
             title: 'L’aéroport d’Entzheim, un pôle d’échange multimodal',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/04_Entzheim.jpg': { "alt": "04_Entzheim", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/04_Entzheim.webp': { "alt": "04_Entzheim", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: "Dans son objectif de « révolution des mobilités », l’Eurométropole de Strasbourg a développé plusieurs projets visant à favoriser les modes de déplacement doux et collectifs afin de lutter contre le dérèglement climatique. Parmi eux, le Réseau express métropolitain (REME), lancé en 2022, a pour objectif de transformer progressivement les 13 gares du territoire en pôles d’échanges multimodaux et à améliorer la fréquence et la cadence de leur desserte en TER. Dans ce contexte, le pôle multimodal de l’aéroport d’Entzheim a mis à disposition des usagers du TER deux de ses parkings, élargissant ainsi l’offre de stationnement multimodal offerte depuis 2008 par la gare d’Entzheim-aéroport, qui dispose également de deux parcs à vélo sécurisés et de multiples connexions avec le réseau de bus CTS.",
             location: {
                 center: [7.61872, 48.5427],
@@ -143,7 +143,7 @@ var config = {
         {
             id: '5',
             title: 'Les paysages forestiers, un atout économique pour la filière bois',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/05_Forets.jpg': { "alt": "05_Forets", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/05_Forets.webp': { "alt": "05_Forets", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'Dans la Vallée de la Bruche, la forêt occupe près de 80% du territoire et bénéficie d’une gestion très majoritairement publique. Avec la remise en cause de la monoculture forestière et la prise en compte de son importance face aux enjeux climatiques, la forêt a repris une place de taille dans les politiques engagées par la Communauté de communes. En 2016, elle a mené une vaste réflexion sur la forêt et le bois avec l’ensemble des acteurs concernés. Celle-ci a mené à l’adoption, en 2021, d’une Stratégie filière forêt-bois, qui propose un programme d’actions portant sur une gestion durable de la forêt, la mise en place de politiques d’utilisation du matériau bois dans la maîtrise d’ouvrage publique, ainsi que la sensibilisation et l’accueil du public.',
             location: {
                 center: [7.331572, 48.52629],
@@ -167,7 +167,7 @@ var config = {
         {
             id: '6',
             title: 'Le site Scheidecker, un projet de réhabilitation d’une friche industrielle',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/06_Scheidecker.jpg': { "alt": "06_Scheidecker", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/06_Scheidecker.webp': { "alt": "06_Scheidecker", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'Village précurseur de l’industrie textile dans la Vallée de la Bruche, Lutzelhouse abrite au milieu du XIXe siècle une filature de coton et de laine peignée, un moulin et une scierie mécanique. A proximité de l’usine, la famille Scheidecker fait ériger un château. En 2022, dans une optique de mise en valeur de ses friches industrielles, l’ancien site Scheidecker est acquis par la Communauté de communes de la Vallée de la Bruche. Depuis, une réflexion est menée sur l’avenir de ce site avec l’ensemble des partenaires de la collectivité, afin de favoriser l’implantation de projets mixtes et innovants sur cet ancien site industriel.',
             location: {
                 center: [7.2864385, 48.515119],
@@ -181,7 +181,7 @@ var config = {
         {
             id: '8',
             title: 'La médiathèque de Wisches, la réhabilitation d’une ancienne gare',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/08_Locomotive.jpg': { "alt": "08_Locomotive", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/08_Locomotive.webp': { "alt": "08_Locomotive", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'La politique paysagère menée par la Communauté de communes de la Vallée de la Bruche a souligné l’importance de la mise en valeur des friches dans les villages. Elles constituent, en effet, une ressource foncière non négligeable pour l’implantation de nouvelles activités, tout en limitant l’étalement urbain ou la disparition des espaces agricoles. Dans cette optique, en 2008, la commune de Wisches rachète à la SNCF l’ancien bâtiment voyageur de la gare, inutilisé depuis les années 1980. Elle inaugure six années plus tard « La Locomotive », un centre culturel composé d’une salle polyvalente accueillant expositions, réunions et activités associatives, ainsi que de logements et d’une nouvelle médiathèque.',
             location: {
                 center: [7.270460, 48.508111],
@@ -243,7 +243,7 @@ var config = {
         {
             id: '10',
             title: 'Les points de lecture du paysage',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/10_PointLecture.jpg': { "alt": "10_PointLecture", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/10_PointLecture.webp': { "alt": "10_PointLecture", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'Le Plan de paysage déployé par la Communauté de communes de la Vallée de la Bruche depuis les années 1990 a participé à mettre en valeur les ressources naturelles, les activités économiques et les modes de vie de ce territoire. Afin de valoriser les actions engagées et d’ouvrir le dialogue entre les acteurs politiques et économiques et les habitants, plusieurs points de lecture du paysage ont été aménagés depuis 2008 à Barembach, Ranrupt ou encore Saâles. Touristes, randonneurs et habitants sont ainsi invités à découvrir certains villages, panoramas, vignes, espaces agricoles et forêts, et à comprendre les enjeux qui les traversent grâce aux passerelles, silhouettes, longues vues et maquettes mis à disposition.',
             location: {
                 center: [7.202478, 48.3700801],
@@ -257,7 +257,7 @@ var config = {
         {
             id: '11',
             title: 'La Bruche, un atout majeur du paysage',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/11_Bruche.jpg': { "alt": "11_Bruche", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/11_Bruche.webp': { "alt": "11_Bruche", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'La Bruche constitue un lien naturel entre les différents territoires urbains et agricoles, de Bourg-Bruche à Strasbourg. Jusque dans les années 1950, elle a constitué une ressource essentielle, tant pour la consommation humaine et les activités agricoles, que pour le flottage des bois vers la plaine d’Alsace ou pour l’énergie hydro-électrique alimentant villages et usines textiles. Après la disparition progressive des aménagements hydrauliques avec la déprise agricole et l’arrivée des énergies fossiles, une attention accrue est à nouveau portée à l’eau présente dans la vallée. Le SCOT de la Bruche (2016) et le Plan climat Bruche Mossig (2022) soulignent ainsi son importance pour la biodiversité et prônent une renaturation des cours d’eau et zones humides. La piste cyclable offre quant à elle de nombreux points de vue sur les paysages bordés par la Bruche.',
             location: {
                 center: [7.14129, 48.38284],
@@ -271,7 +271,7 @@ var config = {
         {
             id: '12',
             title: 'La véloBruche, une piste cyclable de Strasbourg à Saales',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/12_VeloBruche.jpg': { "alt": "12_VeloBruche", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/12_VeloBruche.webp': { "alt": "12_VeloBruche", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'En 2018, la Communauté de communes de la Vallée de la Bruche s’est dotée d’une Stratégie touristique pour un horizon de 10 ans. Le premier axe de cette stratégie concerne les mobilités, ayant pour objectif une « vallée sans voiture ». En complément de la valorisation de la voie ferrée Saint-Dié – Bruche – Strasbourg et de ses nombreuses gares, elle s’appuie sur une mise en valeur du réseau cyclable qui relie Strasbourg à Saales. Pour rendre cet itinéraire plus lisible et l’intégrer au cercle des itinéraires cyclables et véloroutes en Alsace, la « Vélobruche » dispose depuis 2023 de son propre logo. Des investissements sont également prévus pour poursuivre l’équipement du parcours en 2024-2025, incluant signalisation touristique, aire de repos et de service, et création d’une carte cyclo dédiée.<br/><br/>Site internet : <a href="https://www.velo-bruche.fr/">https://www.velo-bruche.fr/</a>',
             location: {
                 center: [7.17202, 48.41833],
@@ -337,7 +337,7 @@ var config = {
         {
             id: '16',
             title: 'Les abbayes des Vosges',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/16_Abbayes.jpg': { "alt": "16_Abbayes", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/16_Abbayes.webp': { "alt": "16_Abbayes", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'Fondées au VII e siècle, les abbayes d’Etival, Moyenmoutier et Senones formaient, avec la cathédrale de Saint-Dié-des-Vosges et l’abbaye aujourd’hui disparue de Bonmoutier, la Sainte-Croix-des-Vosges. Au fil des siècles, elles constituent des lieux de culte, mais aussi d’enseignement, avec l’école de grammaire de Moyenmoutier, et d’activité économique, avec les usines textiles implantées sur le site de l’abbaye de Senones au 19e siècle. Aujourd’hui, cet ensemble religieux et historique s’impose encore dans le paysage, et constitue un attrait touristique et culturel important, entre randonnées, visites guidées et concerts de musique classique, avec le Festival des Abbayes en Lorraine qui fête cette année ses 20 ans.',
             location: {
                 center: [6.92179, 48.38175],
@@ -351,7 +351,7 @@ var config = {
         {
             id: '17',
             title: 'La réhabilitation de la friche du Souche',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/17_Anoud.jpg': { "alt": "17_Anoud", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/17_Anoud.webp': { "alt": "17_Anoud", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'Construites au 19e siècle le long de la rivière de la Meurthe, les papeteries du Souche ont alimenté l’activité économique de la commune d’Anould jusqu’à leur mise en liquidation en 2013. Dès lors, la Communauté d’agglomération de Saint-Dié-des-Vosges et le conseil départemental des Vosges ont engagé des réflexions sur la reconversion du site, qui a laissé en friche près de 30 000 m2 d’infrastructures industrielles. Une douzaine d’hectares est acquise en 2019 par l’Etablissement public foncier du Grand-Est (EPFGE), afin d’amorcer les travaux de désamiantage et développer un projet de reconversion. A terme, le site, qui accueille déjà diverses nouvelles activités industrielles, doit permettre l’établissement d’un centre de formation aux métiers de la sûreté et de la sécurité.',
             location: {
                 center: [6.951643, 48.196479],
@@ -389,7 +389,7 @@ var config = {
         {
             id: '19',
             title: 'Des pôles d’échange multimodaux',
-            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/19_Gare.jpg': { "alt": "19_Gare", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
+            images: { 'https://sig.strasbourg.eu/datastrasbourg/fig2024/images/19_Gare.webp': { "alt": "19_Gare", "credit": '<a href="https://www.adeus.org/">ADEUS</a>' } },
             description: 'Ces dernières années, deux pôles d’échange multimodaux ont été inaugurés dans la Communauté d’agglomération de Saint-Dié-des-Vosges. La gare de Saint-Dié-des-Vosges en 2023, et celle de Raon-l’Etape en 2024, ont ainsi bénéficié de travaux d’envergure visant à revaloriser leur image et à faciliter l’intermodalité. Avec l’aménagement de bornes de recharge électrique, de quais de bus adaptés aux personnes à mobilité réduite, de dépose-minute, de pistes cyclables et de locaux à vélo sécurisés, ces nouveaux aménagements facilitent l’accès à des mobilités douces pour l’ensemble des habitants.',
             location: {
                 center: [6.948121, 48.281898],
