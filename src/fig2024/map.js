357a8112266a43143ec96b19062ce4ec1bb2df85
Julien Monticolo <julien.monticolo@strasbourg.eu>
2024-10-04 14:45:46 +0200

    [fig2024] add 3d wind turbines
    Former-commit-id: 47dea2d734578fdd74705dc38dbc0e2b6163d89c

--- src/fig2024/map.js
+++ src/fig2024/map.js
@@ -268,6 +268,10 @@ var scroller = scrollama();
 tutorial. At the end, however, we setup the functions that will tie the
 scrolling to the chapters and move the map from one location to another
 while changing the zoom level, pitch and bearing */
+
+// Load THREE JS 3d models
+load3dModels(map);
+
 map.on('style.load', async () => {
 
   if (config.layers) {
@@ -275,24 +279,6 @@ map.on('style.load', async () => {
       setLayerOpacity(lyr);
     });
   }
-
-  image = await map.loadImage('https://sig.strasbourg.eu/datastrasbourg/fig2024/images/wind_turbine_2076.png');
-  map.addImage('windturbine', image.data);
-  map.addSource('point', {
-    'type': 'geojson',
-    'data': 'https://sig.strasbourg.eu/datastrasbourg/fig2024/13_Eoliennes.geojson'
-  });
-
-  map.addLayer({
-    'id': 'points',
-    'type': 'symbol',
-    'source': 'point',
-    'layout': {
-      'icon-image': 'windturbine',
-      'icon-allow-overlap': true,
-      'icon-size': ['interpolate', ['linear'], ['zoom'], 10, 0.001, 14, 0.08, 15, 0.1, 18, 0.8, 20, 4, 22, 12]
-    }
-  });
 
   map.addSource('03_Holtzheim', {
     'type': 'image',
